57a29165a7a6765408c53960e958ff71aaf4680a
Ruslan Abramov <abramov-24@yandex.ru>
2024-04-19 11:16:52 +0300

    add BrowserRouter, module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import './App.css';
+import { Profile } from './components/Profile';
+import { Friends } from './components/Friends';
+import { Shop } from './components/Shop';
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="container mt-5">
-      <div className="col-3">
-        <div className="nav flex-column nav-pills">
-          <a href="main" className="nav-link active">Главная</a>
-          <a href="profile" className="nav-link">Профиль</a>
-          <a href="friends" className="nav-link">Друзья</a>
-          <a href="shop" className="nav-link">Магазин</a>
+      <div className="row">
+        <div className="col-3">
+          <div className="nav flex-column nav-pills">
+            <NavLink to="/" className="nav-link">
+              Главная
+            </NavLink>
+            <NavLink to="profile" className="nav-link">
+              Профиль
+            </NavLink>
+            <NavLink to="friends" className="nav-link">
+              Друзья
+            </NavLink>
+            <NavLink to="shop" className="nav-link">
+              Магазин
+            </NavLink>
+          </div>
         </div>
-      </div>
-      <div className="col-9">
-
+        <div className="col-9">
+          <Routes>
+            <Route path="/" element={<h2>Вы в личном кабинете. Выбор в меню слева</h2>} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/friends" element={<Friends />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import styles from "./Profile.module.css";
+
+export const Profile = () => {
+  return (
+    <div className="row">
+      <div className="col-md-4">
+        <img
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=300&q=70"
+          alt=""
+          width="100%"
+        />
+      </div>
+      <div className="col-md-8">
+        <h2 className={styles.header}>
+          Фамилия и имя: <span>Павел Федоров</span>
+        </h2>
+        <p className={styles.about}>О себе...</p>
+        <p className={styles.id}>
+          Id: <span>9</span>
+        </p>
+        <p style={{padding: "10px", border: "1px solid green", borderRadius: "4px"}}>
+          Email: <span>PavelFed@gmail.com</span>
+        </p>
+      </div>
+    </div>
+  );
+};
