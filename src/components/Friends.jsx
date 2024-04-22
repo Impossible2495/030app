497ef054a9054c03b852ce2b0835e887b4f0806b
Ruslan Abramov <abramov-24@yandex.ru>
2024-04-22 11:30:50 +0300

    Friends, Profile, Users

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,10 +1,15 @@
+import { NavLink } from "react-router-dom";
 
 const TableRow = (props) => {
   return (
     <tr>
       <th scope="row">{props.index + 1}</th>
-      <td>{props.lastname}</td>
-      <td>{props.name}</td>
+      <td>
+        <NavLink to={"/profile/" + props.id}>
+          {props.lastname} {props.name}
+        </NavLink>
+      </td>
+      <td>{props.email}</td>
     </tr>
   );
 } 
@@ -17,7 +22,7 @@ export const Friends = (props) => {
   let usersCount = Object.keys(users).length;
   let userRow = [];
   for (let i = 0; i < usersCount; i++) {
-    userRow.push(<TableRow key={i} index={i} name={users[i].name} lastname={users[i].lastname}/>);
+    userRow.push(<TableRow key={i} index={i} id={users[i].id} name={users[i].name} lastname={users[i].lastname} email={users[i].email}/>);
   }
   return (
     <>
@@ -26,13 +31,12 @@ export const Friends = (props) => {
         <table className="table table-striped">
           <thead>
             <tr>
-              <th scope="col">Фамилия</th>
-              <th scope="col">Имя</th>
+              <th scope="col">#</th>
+              <th scope="col">Фамилия и имя</th>
+              <th scope="col">Email</th>
             </tr>
           </thead>
-          <tbody>
-            {userRow}
-          </tbody>
+          <tbody>{userRow}</tbody>
         </table>
       </div>
     </>
--- src/state.jsx
+++ src/state.jsx
@@ -9,17 +9,52 @@ const user = {
 };
 
 const users = {
-  0: { name: "Лена", lastname: "Морозова" },
-  1: { name: "Ульяна", lastname: "Василькова" },
-  2: { name: "Василий", lastname: "Мудрый" },
-  3: { name: "Юля", lastname: "Прекрасная" },
-  4: { name: "Виктория", lastname: "Задорнова" },
+  0: {
+    name: "Лена",
+    lastname: "Морозова",
+    email: "morozova5gmail.com",
+    id: 3,
+    avatar:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+  },
+  1: {
+    name: "Джаред",
+    lastname: "Стоунс",
+    email: "Jared@gmail.com",
+    id: 24,
+    avatar:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+  },
+  2: {
+    name: "Юля",
+    lastname: "Прекрасная",
+    email: "YuliaSun@rocket.com",
+    id: 6,
+    avatar:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",
+  },
+  3: {
+    name: "Василий",
+    lastname: "Мудрый",
+    email: "VasilMudr@mail.ru",
+    id: 13,
+    avatar:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",
+  },
+  4: {
+    name: "Виктория",
+    lastname: "Задорнова",
+    email: "ZadornvovaVika123@gmail.com",
+    id: 42,
+    avatar:
+      "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+  },
 };
 
-
-
-
-export function getUser() {
+export function getUser(userId) {
+  for (let i = 0; i < Object.keys(users).length; i++) {
+    if (users[i].id == userId) return users[i];
+  }
   return user;
 }
 
