05e86d6337431931d9f3e6e4607c6b18834111b2
Ruslan Abramov <abramov-24@yandex.ru>
2024-04-23 12:04:45 +0300

    Advice at Shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Shop = () => {
-  const [count, setCount] = useState(0);
+  const [advice, setAdvice] = useState("");
+  useEffect(() => {
+    const url = "https://api.adviceslip.com/advice";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json.slip.advice);
+        setAdvice(json.slip.advice);
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       <div className="flex flex-col gap-5">
-        <div className="p-6 bg-green-400 rounded-lg text-2xl">Количество кликов {count}</div>
-        <button
-          className="p-6 bg-red-400 text-2xl"
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Нажми
-        </button>
+        <div className="p-6 bg-green-400 rounded-lg text-2xl">Совет: {advice}</div>
+        <button className="p-6 bg-red-400 text-2xl">Нажми</button>
       </div>
     </>
   );
